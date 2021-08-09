d9fc66ddc37310f32e9d619d1000f6171ad4c082
TonyTandAndroid <tonythompsoncmu@gmail.com>
2021-08-09 01:44:00 -0700

    Add java module

--- app/src/main/java/com/demo/hellojitpack/MainActivity.java
+++ app/src/main/java/com/demo/hellojitpack/MainActivity.java
@@ -3,7 +3,7 @@ package com.demo.hellojitpack;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import com.demo.lib1.Lib1Util;
-import com.demo.lib2.Lib2Util;
+import com.demo.lib2.LibJavaUtil;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    System.out.println(Lib1Util.getVersion() + ":" + Lib2Util.getVersion());
+    System.out.println(Lib1Util.getVersion() + ":" + LibJavaUtil.getVersion()+":"+LibJavaUtil.getVersion());
   }
 }
